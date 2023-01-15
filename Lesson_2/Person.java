f0687d2d724c0fba0aac0919716891ff5509033e
Valera Shabanov <github@shasoft.com>
2023-01-15 20:22:51 +0300

    Домашняя работа 2.2

---
+++ Lesson_2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+
+    String gender = "man";
+    String name = "Валера";
+    int height = 180;
+    int weight = 100;
+    int age = 45;
+
+    void go() {
+        System.out.println("go");
+    }
+
+    void sit() {
+        System.out.println("sit");
+    }
+
+    void run() {
+        System.out.println("run");
+    }
+
+    void talk() {
+        System.out.println("talk");
+    }
+
+    void teachJava() {
+        System.out.println("teachJava");
+    }
+}
