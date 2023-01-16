d6c8d67c43722ac3c2120025649a3b471ea996e8
Valera Shabanov <github@shasoft.com>
2023-01-16 19:07:35 +0300

    Задание 2.3

---
+++ Lesson_2/Wolf.java
@@ -0,0 +1,28 @@
+public class Wolf {
+
+    String gender;
+    String name;
+    int weight;
+    int age;
+    String color;
+
+    void go() {
+        System.out.println("Идёт");
+    }
+
+    void sit() {
+        System.out.println("Cидит");
+    }
+
+    void run() {
+        System.out.println("Бежит");
+    }
+
+    void howl() {
+        System.out.println("Воет");
+    }
+
+    void hunt() {
+        System.out.println("Охотится");
+    }
+}
---
+++ Lesson_2/WolfTest.java
@@ -0,0 +1,19 @@
+public class WolfTest {
+    public static void main(String[] args) {
+
+        Wolf wolf = new Wolf();
+        wolf.gender = "man";
+        wolf.name = "Пусик";
+        wolf.weight = 60;
+        wolf.age = 5;
+        wolf.color = "grey";
+
+        System.out.println(wolf.gender + " " + wolf.name + " " + wolf.weight + " " + wolf.age + " " + wolf.color);
+
+        wolf.go();
+        wolf.sit();
+        wolf.run();
+        wolf.howl();
+        wolf.hunt();
+    }
+}
