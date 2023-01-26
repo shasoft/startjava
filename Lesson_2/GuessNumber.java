7faefc0ec568b84d42336ea36fc0a3009c8fd5d9
Valera Shabanov <github@shasoft.com>
2023-01-26 21:05:00 +0300

    Доработка задания 2.6.2

--- Lesson_2/GuessNumber.java
+++ Lesson_2/GuessNumber.java
@@ -2,40 +2,31 @@ import java.util.Scanner;
 
 public class GuessNumber {
     private int secretNumber;
-    private int playerIndex;
     private Player player1;
     private Player player2;
 
     public GuessNumber(Player player1, Player player2) {
-        playerIndex = 0;
         this.player1 = player1;
         this.player2 = player2;
     }
 
     public void start() {
         Scanner console = new Scanner(System.in);
-        secretNumber = (int) (Math.random() * 100.0f);
+        secretNumber = (int) (Math.random() * 101.0f);
+        Player activePlayer = player2;
         while (true) {
-            Player activePlayer;
-            if (playerIndex == 0) {
-                activePlayer = player1;
-                playerIndex = 1;
-            } else {
-                activePlayer = player2;
-                playerIndex = 0;
-            }
-            System.out.print("Игрок " + activePlayer + " укажите число: ");
+            activePlayer = activePlayer.equals(player1) ? player2 : player1;
+            System.out.print("Игрок " + activePlayer + " введите число: ");
             int number = console.nextInt();
             activePlayer.setNumber(number);
             if (number == secretNumber) {
                 System.out.println("Игрок " + activePlayer + " одержал победу");
                 break;
+            }
+            if (number < secretNumber) {
+                System.out.println("Число " + number + " меньше того, что загадал компьютер");
             } else {
-                if (number < secretNumber) {
-                    System.out.println("Число " + number + " меньше того, что загадал компьютер");
-                } else {
-                    System.out.println("Число " + number + " больше того, что загадал компьютер");
-                }
+                System.out.println("Число " + number + " больше того, что загадал компьютер");
             }
         }
     }
