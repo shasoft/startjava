381797290fa857ab1130f9096a8325643d645495
Valera Shabanov <github@shasoft.com>
2023-01-30 19:57:59 +0300

    Доработка задания 4.1 v3

--- src/com/startjava/lesson_2_3_4/array/ArrayTheme.java
+++ src/com/startjava/lesson_2_3_4/array/ArrayTheme.java
@@ -5,42 +5,42 @@ import java.util.Arrays;
 public class ArrayTheme {
     public static void main(String[] args) {
         System.out.println("\n1. Реверс значений массива");
-        int[] intArr = {1, 2, 3, 4, 5, 6, 7};
+        int[] intArr = { 1, 2, 3, 4, 5, 6, 7 };
         printArrayInt("Начальное значение", intArr);
-        for (int i = 0; i < intArr.length / 2; i++) {
+        int len = intArr.length;
+        for (int i = 0; i < len / 2; i++) {
             int temp = intArr[i];
-            intArr[i] = intArr[intArr.length - 1 - i];
-            intArr[intArr.length - 1 - i] = temp;
+            intArr[i] = intArr[len - 1 - i];
+            intArr[len - 1 - i] = temp;
         }
         printArrayInt("После перестановки", intArr);
 
         System.out.println("\n2. Вывод произведения элементов массива");
         System.out.print("\t");
         intArr = new int[10];
-        for (int i = 0; i < intArr.length; i++)
+        len = intArr.length;
+        for (int i = 0; i < len; i++)
             intArr[i] = i;
-        int digits = 1;
-        int countOut = 0;
-        for (int i = 0; i < intArr.length; i++) {
-            boolean isMul = intArr[i] > 0 && intArr[i] < 9;
-            digits *= isMul ? intArr[i] : 1;
-            countOut += isMul ? 1 : 0;
-            System.out.print(isMul ? (intArr[i] + (isMul ? (" " + (countOut < 8 ? "*" : "=") + " ") : "")) : "");
+        int productDigits = 1;
+        for (int i = 1; i < len - 1; i++) {
+            productDigits *= intArr[i];
+            System.out.print(intArr[i] + " " + (i < 8 ? "*" : "=") + " ");
         }
-        System.out.println(digits);
+        System.out.println(productDigits);
         System.out.println("\tЭлемент массив 0 = " + intArr[0]);
         System.out.println("\tЭлемент массив 9 = " + intArr[9]);
 
         System.out.println("\n3. Удаление элементов массива");
         float[] floatArr = new float[15];
-        for (int i = 0; i < floatArr.length; i++) {
+        len = floatArr.length;
+        for (int i = 0; i < len; i++) {
             floatArr[i] = (float) Math.random();
         }
         printArrayFloat("Исходный массив", floatArr, 8);
-        float middleNumber = floatArr[floatArr.length / 2];
+        float middleNumber = floatArr[len / 2];
         System.out.printf("\tСреднее значение %.3f\n", middleNumber);
         int countZero = 0;
-        for (int i = 0; i < floatArr.length; i++) {
+        for (int i = 0; i < len; i++) {
             if (middleNumber < floatArr[i]) {
                 floatArr[i] = 0;
                 countZero++;
@@ -50,53 +50,56 @@ public class ArrayTheme {
         System.out.println("\tколичество обнуленных ячеек " + countZero);
 
         System.out.println("\n4. Вывод элементов массива лесенкой в обратном порядке");
-        char[] arrChars = new char['Z' - 'A' + 1];
+        char[] englishLetters = new char['Z' - 'A' + 1];
         for (char ch = 'A'; ch <= 'Z'; ch++) {
-            arrChars[ch - 'A'] = ch;
+            englishLetters[ch - 'A'] = ch;
         }
-        for (int row = 0; row < arrChars.length; row++) {
+        len = englishLetters.length;
+        for (int row = 0; row < len; row++) {
             System.out.print("\t");
             for (int col = 0; col <= row; col++) {
-                System.out.print(arrChars[arrChars.length - 1 - col]);
+                System.out.print(englishLetters[len - 1 - col]);
             }
             System.out.println();
         }
 
         System.out.println("\n5. Генерация уникальных чисел");
-        int[] intArrRandom = new int[41];
-        for (int i = 0; i < intArrRandom.length; i++) {
-            intArrRandom[i] = 60 + i;
+        int[] uniqueNumbers = new int[30];
+        int countNumbers = 0;
+        while (countNumbers < uniqueNumbers.length) {
+            uniqueNumbers[countNumbers] = (int) (Math.random() * 40 + 60);
+            boolean hasAppend = true;
+            for (int i = 0; i < countNumbers - 1; i++) {
+                if (uniqueNumbers[countNumbers] == uniqueNumbers[i]) {
+                    hasAppend = false;
+                    break;
+                }
+            }
+            if (hasAppend)
+                countNumbers++;
         }
-        intArr = new int[30];
-        int countRandom = intArrRandom.length;
-        for (int i = 0; i < intArr.length; i++) {
-            int indexRandom = (int) (Math.random() * (float) countRandom);
-            intArr[i] = intArrRandom[indexRandom];
-            intArrRandom[indexRandom] = intArrRandom[countRandom - 1];
-            countRandom--;
-        }
-        for (int i = 0; i < intArr.length - 1; i++) {
-            for (int j = i + 1; j < intArr.length; j++) {
-                if (intArr[i] > intArr[j]) {
-                    int temp = intArr[i];
-                    intArr[i] = intArr[j];
-                    intArr[j] = temp;
+        for (int i = 0; i < uniqueNumbers.length - 1; i++) {
+            for (int j = i + 1; j < uniqueNumbers.length; j++) {
+                if (uniqueNumbers[i] > uniqueNumbers[j]) {
+                    int temp = uniqueNumbers[i];
+                    uniqueNumbers[i] = uniqueNumbers[j];
+                    uniqueNumbers[j] = temp;
                 }
 
             }
         }
-        for (int i = 0; i < intArr.length; i++) {
+        for (int i = 0; i < uniqueNumbers.length; i++) {
             if (i % 10 == 0) {
                 if (i != 0)
                     System.out.println();
                 System.out.print("\t");
             }
-            System.out.printf("%4d", intArr[i]);
+            System.out.printf("%4d", uniqueNumbers[i]);
         }
         System.out.println();
 
         System.out.println("\n6. Сдвиг элементов массива");
-        String[] strings = {"    ", "AA", "", "BBB", "CC", "D", "    ", "E", "FF", "G", ""};
+        String[] strings = { "    ", "AA", "", "BBB", "CC", "D", "    ", "E", "FF", "G", "" };
         System.out.println("\tИсходный массив: " + Arrays.toString(strings));
         int countNotBlank = 0;
         for (String str : strings) {
