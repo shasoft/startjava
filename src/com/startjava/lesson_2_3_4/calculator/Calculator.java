d21354977677e9bb410b3a003c797f9c2f3a9601
Valera Shabanov <github@shasoft.com>
2023-02-03 21:15:18 +0300

    4.6.1*

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -2,34 +2,22 @@ package com.startjava.lesson_2_3_4.calculator;
 
 public class Calculator {
 
-    public double calculate(String expression) {
-        String[] portions = expression.split(" ", 3);
-        double number1 = (double) Integer.parseInt(portions[0]);
-        double number2 = (double) Integer.parseInt(portions[2]);
-        double result = 0f;
-        switch (portions[1].charAt(0)) {
-            case '+':
-                result = number1 + number2;
-                break;
-            case '-':
-                result = number1 - number2;
-                break;
-            case '*':
-                result = number1 * number2;
-                break;
-            case '/':
-                result = number1 / number2;
-                break;
-            case '^':
-                result = Math.pow(number1, number2);
-                break;
-            case '%':
-                result = number1 % number2;
-                break;
-            default:
-                System.out.println("Операция не поддерживается!");
+    static public double calculate(String expression) {
+        String[] partsExpression = expression.split(" ", 3);
+        try {
+            double number1 = (double) Integer.parseInt(partsExpression[0]);
+            double number2 = (double) Integer.parseInt(partsExpression[2]);
+            return switch (partsExpression[1].charAt(0)) {
+                case '+' -> (number1 + number2);
+                case '-' -> number1 - number2;
+                case '*' -> number1 * number2;
+                case '/' -> number1 / number2;
+                case '^' -> Math.pow(number1, number2);
+                case '%' -> number1 % number2;
+                default -> throw new RuntimeException("Операция не поддерживается!");
+            };
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException("Неверный формат ввода выражения!");
         }
-        return result;
     }
-
 }
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -5,24 +5,24 @@ import java.util.Scanner;
 public class CalculatorTest {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
-
-        Calculator calculator = new Calculator();
         String answer = "yes";
         do {
-            if (answer.equals("yes")) {
-                System.out.print("Введите математическое выражение: ");
+            try {
+                if (answer.equals("yes")) {
+                    System.out.print("Введите математическое выражение: ");
+                    double result = Calculator.calculate(console.nextLine());
+                    System.out.print("Результат операции = ");
+                    if (result % 1.0 != 0.0)
+                        System.out.printf("%f", result);
+                    else
+                        System.out.printf("%.0f", result);
+                    System.out.println();
+                }
+                System.out.print("Хотите продолжить вычисления? [yes/no]: ");
                 answer = console.nextLine();
-                double result = calculator.calculate(answer);
-                System.out.print("Результат операции = ");
-                if (result % 1.0 != 0.0)
-                    System.out.printf("%f", result);
-                else
-                    System.out.printf("%.0f", result);
-                System.out.println();
+            } catch (RuntimeException e) {
+                System.out.println("Ошибка: " + e.getMessage());
             }
-            System.out.print("Хотите продолжить вычисления? [yes/no]: ");
-            answer = console.nextLine();
-
         } while (!answer.equals("no"));
     }
 }
