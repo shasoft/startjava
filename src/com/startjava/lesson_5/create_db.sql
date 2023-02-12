6bab97c74a54baa7929290e56a5caea77e5fc614
Valera Shabanov <github@shasoft.com>
2023-02-12 14:33:14 +0300

    Последние правки

--- src/com/startjava/lesson_5/create_db.sql
+++ src/com/startjava/lesson_5/create_db.sql
@@ -3,20 +3,22 @@
 
 /* Создать базу данных Jaegers */
 DROP DATABASE IF EXISTS Jaegers;
+
 CREATE DATABASE Jaegers;
 
 /* Создать базу данных Jaegers */
 DROP TABLE IF EXISTS Jaegers;
 CREATE TABLE Jaegers (
-  id SERIAL PRIMARY KEY,
-  modelName TEXT,
-  mark CHAR(6),
-  height INTEGER,
-  weight INTEGER,
-  status CHAR(16),
-  origin CHAR(64),
-  launch DATE,
-  kaijuKill INTEGER);
+    id SERIAL PRIMARY KEY,
+    modelName TEXT,
+    mark CHAR(6),
+    height INTEGER,
+    weight INTEGER,
+    status CHAR(16),
+    origin CHAR(64),
+    launch DATE,
+    kaijuKill INTEGER
+);
 
 \ir init_db.sql
 \ir queries.sql
